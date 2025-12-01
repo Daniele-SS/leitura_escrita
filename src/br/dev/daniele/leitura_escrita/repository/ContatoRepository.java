d7c02084659e617d2e0aeec7e21ca18f5df909a4
DANIELE SILVA SANTOS <25203622@dsn127m1247444.cfp127aluno.local>
2025-12-01 09:47:07 -0300

    adicionada a URI do arquivo

--- src/br/dev/daniele/leitura_escrita/repository/ContatoRepository.java
+++ src/br/dev/daniele/leitura_escrita/repository/ContatoRepository.java
@@ -1,4 +1,13 @@
 package br.dev.daniele.leitura_escrita.repository;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class ContatoRepository {
+    public Contato contato;
+
+    public void gravar(){
+        Path file = Paths.get("/Users/25203622/Documents/Arquivos/dados.csv");
+
+    }
 }
